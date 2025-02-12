a1d8e809bcd74a18b0325a02ec315d126a616a7e
Abdullah M. Bakry <abdaullah62@gmail.com>
2025-02-12 13:17:48 +0200

    Add PageContainer component and 404 Not Found page; include nextjs-toploader in package.json

---
+++ components/layout/page-container.tsx
@@ -0,0 +1,30 @@
+'use client';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { cn } from '@/lib/utils';
+import React from 'react';
+
+export default function PageContainer({
+    children,
+    scrollable = true,
+    className,
+}: {
+    children: React.ReactNode;
+    scrollable?: boolean;
+    className?: string;
+}) {
+    return (
+        <>
+            {scrollable ? (
+                <ScrollArea>
+                    <div className={cn('flex flex-1 p-4 md:px-6', className)}>
+                        {children}
+                    </div>
+                </ScrollArea>
+            ) : (
+                <div className={cn('flex flex-1 p-4 md:px-6', className)}>
+                    {children}
+                </div>
+            )}
+        </>
+    );
+}
