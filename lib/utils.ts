01c71afff4ddd785d9e325a21339bd2336b259d0
Abdullah M. Bakry <abdaullah62@gmail.com>
2025-02-12 15:33:52 +0200

    Add truncateString utility function to handle string truncation

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
 }
+
+export const truncateString = (str: string, num: number) => {
+    if (!str || typeof str !== 'string') {
+        return '';
+    }
+
+    if (str.length <= num) {
+        return str;
+    }
+    return str.slice(0, num) + '...';
+};
