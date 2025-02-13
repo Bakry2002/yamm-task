94003a6078ac36f5e99e48cdc4cf80c7a4fbdd9b
Abdullah M. Bakry <abdaullah62@gmail.com>
2025-02-13 11:47:50 +0200

    Refactor OrderDecisionSwitcher to include decision color logic and remove redundant utility function

--- app/refund_orders/_components/order-decision-switcher.tsx
+++ app/refund_orders/_components/order-decision-switcher.tsx
@@ -6,7 +6,7 @@ import {
     SelectValue,
 } from '@/components/ui/select';
 import { useChangeRefundOrderDecision } from '@/lib/services/mutations';
-import { cn, formatEnum, getDecisionColor } from '@/lib/utils';
+import { cn, formatEnum } from '@/lib/utils';
 import { DecicionType } from '@prisma/client';
 
 import {
@@ -55,6 +55,21 @@ export const OrderDecisionSwitcher = ({
             case 'ESCALATED':
                 return <CircleArrowUpIcon className="h-4 w-4" />;
             default:
+        }
+    };
+
+    const getDecisionColor = (decision: string) => {
+        if (!decision)
+            return 'bg-gray-100 text-gray-800 hover:bg-gray-200 border border-gray-400';
+        switch (decision) {
+            case 'ACCEPTED':
+                return 'bg-green-100 text-green-800 hover:bg-green-200 border border-green-400';
+            case 'ESCALATED':
+                return 'bg-blue-100 text-blue-800 hover:bg-blue-200 border border-blue-400';
+            case 'PENDING':
+                return 'bg-yellow-100 text-yellow-800 hover:bg-yellow-200 border border-yellow-400';
+            default:
+                return 'bg-gray-100 text-gray-800 hover:bg-gray-200 border border-gray-400';
         }
     };
     return (
--- lib/utils.ts
+++ lib/utils.ts
@@ -16,21 +16,6 @@ export const truncateString = (str: string, num: number) => {
     return str.slice(0, num) + '...';
 };
 
-export const getDecisionColor = (decision: string) => {
-    if (!decision)
-        return 'bg-gray-100 text-gray-800 hover:bg-gray-200 border border-gray-400';
-    switch (decision) {
-        case 'Completed':
-            return 'bg-green-100 text-green-800 hover:bg-green-200 border border-green-400';
-        case 'In Progress':
-            return 'bg-blue-100 text-blue-800 hover:bg-blue-200 border border-blue-400';
-        case 'Pending':
-            return 'bg-yellow-100 text-yellow-800 hover:bg-yellow-200 border border-yellow-400';
-        default:
-            return 'bg-gray-100 text-gray-800 hover:bg-gray-200 border border-gray-400';
-    }
-};
-
 export const formatEnum = (enumValue: string) => {
     return enumValue
         .toLowerCase()
