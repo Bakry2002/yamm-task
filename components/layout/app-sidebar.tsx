a01edb1b0541695b220bb13ee4a1ba8937dd2343
Abdullah M. Bakry <abdaullah62@gmail.com>
2025-02-13 11:47:40 +0200

    Replace company logo with SVG file and update sidebar component to use Image for rendering

--- components/layout/app-sidebar.tsx
+++ components/layout/app-sidebar.tsx
@@ -19,14 +19,15 @@ import {
     SidebarRail,
 } from '@/components/ui/sidebar';
 import { navItems } from '@/constants';
-import { ChevronRight, GalleryVerticalEnd } from 'lucide-react';
+import { ChevronRight } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { Icons } from '../icons';
 
 export const company = {
     name: 'Yamm',
-    logo: GalleryVerticalEnd,
+    logo: '/yamm_logo.svg',
     plan: 'Enterprise',
 };
 
@@ -37,8 +38,13 @@ export default function AppSidebar({}) {
         <Sidebar collapsible="icon" className="rtl:right-0">
             <SidebarHeader>
                 <div className="flex gap-2 py-2 text-sidebar-accent-foreground">
-                    <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                        <company.logo className="size-4" />
+                    <div className="flex aspect-square size-8 items-center justify-center rounded-lg border text-sidebar-primary-foreground">
+                        <Image
+                            src={company.logo}
+                            alt="Company Logo"
+                            width={30}
+                            height={30}
+                        />
                     </div>
                     <div className="grid flex-1 text-left text-sm leading-tight rtl:text-right">
                         <span className="truncate font-semibold">
